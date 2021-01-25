5ebe08b49b9950c48d9b60bedb2924c888d62189
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-25 17:51:23 -0300

    First commit

---
+++ src/main/java/com/example/SystemFinalSpring/SystemFinalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SystemFinalSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemFinalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemFinalSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SystemFinalSpring/SystemFinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SystemFinalSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemFinalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
