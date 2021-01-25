3f88d3d8abe2077ad4c9163d5a798e94be613b48
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-25 19:28:52 -0300

    Criada tabela conteudo na database system_final_spring

---
+++ src/main/java/com/example/SystemFinalSpring/entity/Conteudo.java
@@ -0,0 +1,53 @@
+package com.example.SystemFinalSpring.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Conteudo implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="native")
+    @GenericGenerator(name="native", strategy = "native")
+    private long codigo;
+
+    @Column
+    private String Material;
+    @Column
+    private int quantPalavras;
+    @Column
+    private int quantCaracteres;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMaterial() {
+        return Material;
+    }
+
+    public void setMaterial(String material) {
+        Material = material;
+    }
+
+    public int getQuantPalavras() {
+        return quantPalavras;
+    }
+
+    public void setQuantPalavras(int quantPalavras) {
+        this.quantPalavras = quantPalavras;
+    }
+
+    public int getQuantCaracteres() {
+        return quantCaracteres;
+    }
+
+    public void setQuantCaracteres(int quantCaracteres) {
+        this.quantCaracteres = quantCaracteres;
+    }
+}
